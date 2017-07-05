96e50a64a04a66740e9bdacb6bac82d31963499c
Diogo Bittencourt <diogochbittencourt@gmail.com>
2017-07-05 09:21:18 -0300

    Getting Started with Kotlin - Pluralsight course.

---
+++ classesandobjects/src/main/kotlin/com/github/diogochbittencourt/ps/Person.kt
@@ -0,0 +1,14 @@
+package com.github.diogochbittencourt.ps
+
+interface Signatory{
+    fun sign()
+}
+
+class Person : Signatory {
+    override fun sign() = println("I can sign documents")
+}
+
+fun main (args: Array<String>){
+    val p = Person()
+    p.sign()
+}
