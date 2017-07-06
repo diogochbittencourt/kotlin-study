3e0516565362a7b42ab5da6e80f4fb8b3e191f0a
Diogo Bittencourt <diogochbittencourt@gmail.com>
2017-07-06 09:28:44 -0300

    Getting Started with Kotlin - Pluralsight course.

--- classesandobjects/src/main/kotlin/com/github/diogochbittencourt/ps/Person.kt
+++ classesandobjects/src/main/kotlin/com/github/diogochbittencourt/ps/Person.kt
@@ -1,14 +1,47 @@
 package com.github.diogochbittencourt.ps
 
-interface Signatory{
+interface Signatory {
     fun sign()
 }
 
-class Person : Signatory {
-    override fun sign() = println("I can sign documents")
+open class Person(val name: String, var age: Int, var isMarried: Boolean = false) : Signatory {
+//    var isMarried = false
+
+    var partnerName: String =""
+
+//    constructor(name: String, age: Int, isMarried: Boolean) : this(name, age) {
+//        this.isMarried = isMarried
+//    }
+
+    init {
+        if (name == "Kevin" && age > 80) throw Exception("Invalid age")
+    }
+
+    override fun sign() = println("$name aged $age can sign documents (and theur marital status is $isMarried) and his partner name is $partnerName")
+
+//    companion object {
+//        @JvmStatic
+//        fun main(args: Array<String>) {
+//            val p = Person("Kevin", 21, true)
+//            p.sign()
+//            p.age = 54
+//            p.sign()
+//        }
+//    }
 }
 
-fun main (args: Array<String>){
-    val p = Person()
+class Student(name: String, age: Int) : Person(name, age)
+
+fun main(args: Array<String>) {
+    val p = Person("Kevin", 21, true)
     p.sign()
+    p.age = 54
+    p.partnerName = "Alice"
+    p.sign()
+
+    print("${p.partnerName} is his partner.")
+
+    p.partnerName = "Bob"
+
+    print("\n${p.partnerName} is his partner.")
 }
