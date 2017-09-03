30b458105fcb2d84a08c3b051b3973bbc4339a2d
Diogo Bittencourt <diogochbittencourt@gmail.com>
2017-09-03 16:59:59 -0300

    Learning Kotlin.

--- classesandobjects/src/main/kotlin/com/github/diogochbittencourt/ps/Person.kt
+++ classesandobjects/src/main/kotlin/com/github/diogochbittencourt/ps/Person.kt
@@ -32,16 +32,35 @@ open class Person(val name: String, var age: Int, var isMarried: Boolean = false
 
 class Student(name: String, age: Int) : Person(name, age)
 
+data class User(val name: String, val id: Int)
+
 fun main(args: Array<String>) {
+//    val p = Person("Kevin", 21, true)
+//    p.sign()
+//    p.age = 54
+//    p.partnerName = "Alice"
+//    p.sign()
+//
+//    print("${p.partnerName} is his partner.")
+//
+//    p.partnerName = "Bob"
+//
+//    print("\n${p.partnerName} is his partner.")
+
     val p = Person("Kevin", 21, true)
     p.sign()
     p.age = 54
-    p.partnerName = "Alice"
     p.sign()
 
-    print("${p.partnerName} is his partner.")
+    val kevin = User("Kevin", 1)
 
-    p.partnerName = "Bob"
+    println(kevin)
 
-    print("\n${p.partnerName} is his partner.")
+    val (name, id) = kevin
+
+    val otherUser = kevin.copy(id = 2)
+
+    println("User is $name and their id is $id")
+    println(otherUser)
+
 }
